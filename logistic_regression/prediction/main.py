1b922f49834d632385c692f43184f1e79703dc47
imaiyu625 <yusaku.imai.625@gmail.com>
2022-09-05 22:51:52 +0900

    rename func

--- logistic_regression/prediction/main.py
+++ logistic_regression/prediction/main.py
@@ -25,14 +25,14 @@ AIP_PREDICT_ROUTE = os.environ.get('AIP_PREDICT_ROUTE', '/predict')
 async def health():
     return 200
 
-def log_reg(x_):
+def logistic_regression(x_):
     x_ = np.concatenate((np.ones((x_.shape[0], 1)), x_), axis=1) # add bias term
     return 1 / (1 + np.exp(-np.dot(x_, param)))
 
 @app.post(AIP_PREDICT_ROUTE)
 async def predict(request: Request):
     body = await request.json()
-    response = log_reg(np.array(body['instances']))
+    response = logistic_regression(np.array(body['instances']))
     if isinstance(response, np.ndarray):
         return {'predictions': response.tolist()}
     else:
