a71568f8afe7fc8df033d177e595c3965d09c7d8
imaiyu625 <yusaku.imai.625@gmail.com>
2022-09-06 13:37:43 +0900

    refactoring

--- factorization_machines/prediction/main.py
+++ factorization_machines/prediction/main.py
@@ -30,11 +30,13 @@ def factorization_machines(x_):
     w_, v_ = np.hsplit(param, [1])
     v_ = np.vsplit(v_, [1])[1] # remove bias term
     wx_ = np.dot(x_, w_.T.flatten())
+
+    v_ij = np.triu(np.dot(v_, v_.T), k=1)
+
     vx_ = list()
     for x in x_.reshape(x_.shape[0], 1, x_.shape[1]):
         x = np.hsplit(x, [1])[1] # remove bias term
         x_ij = np.tril(np.dot(x.T, x), k=-1)
-        v_ij = np.triu(np.dot(v_, v_.T), k=1)
         vx_.append(np.diag(np.dot(v_ij, x_ij)).sum())
     return 1 / (1 + np.exp(-(wx_ + vx_)))
 
