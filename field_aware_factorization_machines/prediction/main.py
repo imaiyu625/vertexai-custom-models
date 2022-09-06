f0a8a04b0f3905d5fae3eadd392a42f42e26bd73
imaiyu625 <yusaku.imai.625@gmail.com>
2022-09-06 16:02:09 +0900

    add ffm

---
+++ field_aware_factorization_machines/prediction/main.py
@@ -0,0 +1,45 @@
+import os
+import pickle
+from google.cloud import storage
+from urllib.parse import urlparse
+import numpy as np
+from fastapi import Request, FastAPI
+
+artifact_filename = 'model.pkl'
+model_directory = os.environ['AIP_STORAGE_URI']
+storage_path = os.path.join(model_directory, artifact_filename)
+
+storage_client = storage.Client()
+
+parsed_url = urlparse(storage_path)
+bucket = storage_client.bucket(parsed_url.netloc)
+blob = bucket.blob(parsed_url.path.lstrip('/'))
+param = pickle.loads(blob.download_as_string())
+
+app = FastAPI()
+
+AIP_HEALTH_ROUTE = os.environ.get('AIP_HEALTH_ROUTE', '/health')
+AIP_PREDICT_ROUTE = os.environ.get('AIP_PREDICT_ROUTE', '/predict')
+
+@app.get(AIP_HEALTH_ROUTE)
+async def health():
+    return 200
+
+def field_aware_factorization_machines(x_):
+    x_ = np.concatenate((np.ones((x_.shape[0], 1)), x_), axis=1) # add bias term
+    wx_ = np.dot(param['w_'], x_.T)
+    vx_ = list()
+    for x in x_.reshape(x_.shape[0], 1, x_.shape[1]):
+        x = np.hsplit(x, [1])[1] # remove bias term
+        x_ij = np.tril(np.dot(x.T, x), k=-1)
+        vx_.append(np.diag(np.dot(param['v_ij'], x_ij)).sum())
+    return 1 / (1 + np.exp(-(wx_ + vx_)))
+
+@app.post(AIP_PREDICT_ROUTE)
+async def predict(request: Request):
+    body = await request.json()
+    response = field_aware_factorization_machines(np.array(body['instances']))
+    if isinstance(response, np.ndarray):
+        return {'predictions': response.tolist()}
+    else:
+        return 500
---
+++ field_aware_factorization_machines/training/main.py
@@ -0,0 +1,69 @@
+import os
+import pickle
+import tempfile
+from google.cloud import storage
+from urllib.parse import urlparse
+from io import BytesIO
+import argparse
+import pandas as pd
+import numpy as np
+import scipy.optimize as optimize
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input', type=str)
+parser.add_argument('--output', type=str)
+parser.add_argument('--nfactors', type=int, default=4)
+args = parser.parse_args()
+
+storage_client = storage.Client()
+
+parsed_url = urlparse(args.input)
+bucket = storage_client.bucket(parsed_url.netloc)
+blob = bucket.blob(parsed_url.path.lstrip('/'))
+
+def __parse_param(w_, x_):
+    w_, v_ = np.hsplit(w_.reshape((x_.shape[1], (args.nfactors * (x_.shape[1] - 1) + 1))), [1])
+    v_ = np.vsplit(v_, [1])[1] # remove bias term
+    v_ = v_.reshape((x_.shape[1] - 1, x_.shape[1] - 1, args.nfactors))
+    v_ij = np.zeros((x_.shape[1] - 1, x_.shape[1] - 1))
+    for i in range(x_.shape[1] - 1):
+        for j in range(i + 1, x_.shape[1] - 1):
+            v_ij[i,j] = np.dot(v_[i,j], v_[j,i].T)
+    return w_.T.flatten(), v_ij
+
+def loss_func(w_, x_, y_):
+    w_, v_ij = __parse_param(w_, x_)
+    wx_ = np.dot(w_, x_.T)
+    vx_ = list()
+    for x in x_.reshape(x_.shape[0], 1, x_.shape[1]):
+        x = np.hsplit(x, [1])[1] # remove bias term
+        x_ij = np.tril(np.dot(x.T, x), k=-1)
+        vx_.append(np.diag(np.dot(v_ij, x_ij)).sum())
+    p_ = 1 / (1 + np.exp(-(wx_ + vx_)))
+    ll = np.dot(y_.T, np.log(p_)) + np.dot(1 - y_.T, np.log(1 - p_))
+    return - ll / len(y_)
+
+def fit(df):
+    y_ = df[0].to_numpy()
+    x_ = df.drop(0, axis=1).to_numpy()
+    x_ = np.concatenate((np.ones((x_.shape[0], 1)), x_), axis=1) # add bias term
+    np.random.seed(seed=123)
+    w_ = np.random.normal(0, 1e-3, x_.shape[1] * (args.nfactors * (x_.shape[1] - 1) + 1))
+    print('negative log likelihood:', loss_func(w_, x_, y_))
+    wopt_ = optimize.minimize(loss_func, w_, args=(x_, y_), method='L-BFGS-B').x
+    print('negative log likelihood:', loss_func(wopt_, x_, y_))
+    w_, v_ij = __parse_param(wopt_, x_)
+    return {'w_': w_, 'v_ij': v_ij}
+
+raw_data = pd.read_csv(BytesIO(blob.download_as_bytes()), header=None)
+param = fit(raw_data)
+
+model_directory = os.environ.get('AIP_MODEL_DIR', args.output)
+artifact_filename = 'model.pkl'
+storage_path = os.path.join(model_directory, artifact_filename)
+
+with tempfile.NamedTemporaryFile(mode='wb') as temp:
+    pickle.dump(param, temp)
+    temp.flush()
+    blob = storage.blob.Blob.from_string(storage_path, client=storage_client)
+    blob.upload_from_filename(temp.name)
